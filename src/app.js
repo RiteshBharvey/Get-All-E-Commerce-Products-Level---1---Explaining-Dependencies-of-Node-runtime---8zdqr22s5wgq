04b521447b8052edeba15c37322b2660560e467c
RiteshBharvey <99973928+RiteshBharvey@users.noreply.github.com>
2023-04-07 23:59:32 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -12,14 +12,14 @@ app.use(express.json());
 // Endpoint - /api/v1/products
 
 app.get("/api/v1/products",(req,res,next)=>{
-    if(products){
-        if(products.length>0){
+    //if(products){
+        //if(products.length>0){
             res.status(200).json({status: "success",message: "Product fetched successfully", data :{product: products}});
-        }else{
-            res.status(404).json({message: "Product not found"});
-        }
-    }else{
-        res.status(404).json({message: "Product not found"});
-    }
+        //}else{
+            //res.status(404).json({message: "Product not found"});
+        //}
+    //}else{
+       // res.status(404).json({message: "Product not found"});
+    // }
 })
 module.exports = app;
