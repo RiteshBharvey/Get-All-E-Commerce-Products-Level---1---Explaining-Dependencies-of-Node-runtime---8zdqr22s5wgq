d55760149cd93740c0dbcf38f452f5f6b7e5edb2
RiteshBharvey <99973928+RiteshBharvey@users.noreply.github.com>
2023-04-08 00:13:59 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -12,18 +12,10 @@ app.use(express.json());
 // Endpoint - /api/v1/products
 
 app.get("/api/v1/products",(req,res,next)=>{
-    try{
-        //if(products){
-        //if(products.length>0){
-            res.status(200).json({status: "success",message: "Product fetched successfully", data :{product: products}});
-        //}else{
-            //res.status(404).json({message: "Product not found"});
-        //}
-    //}else{
-       // res.status(404).json({message: "Product not found"});
-    // }
-    }catch(err){
-        cosole.log(err);
+    if(products.length>0){
+        res.status(200).json({status: "success",message: "Product fetched successfully", data :{product: products}});
+    }else{
+        res.status(404).json({message: "Product not found"});
     }
 })
 module.exports = app;
